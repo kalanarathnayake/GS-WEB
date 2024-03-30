cd8f8a72b4711930b25bcd6c53d5b043005f3225
kalanaRathnayake <kalanagayanga8@cf.lk>
2024-03-30 18:31:05 +0530

    fix

--- src/components/People/people-list.component.js
+++ src/components/People/people-list.component.js
@@ -13,21 +13,21 @@ export default function PeopleList() {
     // const [birthdate, setBirthDate] = useState('');
 
     useEffect(() => {
-        axios.get(`http://localhost:5000/people/`)
+        axios.get(`https://gs-service.onrender.com/people/`)
             .then((response) => {
                 setAPIData(response.data);
             })
     }, [])
 
     const getData = () => {
-        axios.get(`http://localhost:5000/people/`)
+        axios.get(`https://gs-service.onrender.com/people/`)
             .then((getData) => {
                 setAPIData(getData.data);
             })
     }
 
     const onDelete = (id) => {
-        axios.delete(`http://localhost:5000/people/${id}`).then(response => {
+        axios.delete(`https://gs-service.onrender.com/people/${id}`).then(response => {
             console.log(response.status)
             if (response.status === 200) {
                 Swal.fire({
@@ -213,7 +213,7 @@ export default function PeopleList() {
             <>
                 {show && (
                     <div id="static-modal" data-modal-backdrop="static" tabIndex="-1" aria-hidden="true" className=" fixed overflow-scroll top-0 right-0 left-0 z-50 flex justify-center items-center w-full h-screen bg-gray-800 bg-opacity-80">
-                        <div className="mt-28 relative p-4 max-w-2xl">
+                        <div className="mt-[30rem] relative p-4 max-w-2xl">
                             <div className="relative bg-white rounded-lg shadow">
                                 <div className="flex items-center justify-between p-2 border-b rounded-t">
                                     <h3 className="text-2xl  font-bold text-black ">
@@ -423,13 +423,13 @@ export default function PeopleList() {
                                 </div>
                                 <div className="flex items-center p-4 border-t">
 
-                                    <button className='inline-flex items-center px-4 py-2 ml-1 text-sm font-medium text-white duration-100 bg-green-500 rounded-md hover:bg-blue-200' onClick={handleClose}>
+                                    <button className='inline-flex items-center pl-5 py-2 ml-1 text-sm font-medium text-white duration-100 bg-green-500 rounded-md hover:bg-blue-200' onClick={handleClose}>
                                         <div class=" grid grid-cols-2 gap-1 hover:text-black duration-100">
-                                            <div class="">
+                                            {/* <div class="">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
                                                 </svg>
-                                            </div>
+                                            </div> */}
                                             <div className='self-center text-lg'>
                                                 Close
                                             </div>
@@ -437,14 +437,14 @@ export default function PeopleList() {
                                     </button>
                                     <div class="">
                                         <Link to='/editBudget'>
-                                            <button className='inline-flex items-center px-4 py-2 ml-1 text-sm font-medium text-white duration-100 bg-blue-700 rounded-md hover:bg-blue-200' onClick={() => setData(data)}>
-                                                <div class=" grid grid-cols-2 hover:text-black duration-100">
-                                                    <div class="">
+                                            <button className='inline-flex justify-center items-center pl-5 py-2 ml-1 text-sm font-medium text-white duration-100 bg-blue-700 rounded-md hover:bg-blue-200' onClick={() => setData(data)}>
+                                                <div class=" grid grid-cols-2  hover:text-black duration-200">
+                                                    {/* <div class="">
                                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                                             <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                                                         </svg>
 
-                                                    </div>
+                                                    </div> */}
                                                     <div class="self-center text-lg">
                                                         Update
                                                     </div>
@@ -455,15 +455,15 @@ export default function PeopleList() {
                                     {/* <button onClick={handleClose} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5">I accept</button> */}
                                     {/* <button onClick={handleClose} type="button" className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">CLOSE</button> */}
                                     <div className="">
-                                        <button className='inline-flex items-center px-4 py-2 ml-1 text-sm font-medium text-white duration-100 bg-red-700 rounded-md hover:bg-blue-200' onClick={() => onDelete(data._id)} closeModal >
+                                        <button className='inline-flex items-center pl-5 py-2 ml-1 text-sm font-medium text-white duration-100 bg-red-700 rounded-md hover:bg-blue-200' onClick={() => onDelete(data._id)} closeModal >
                                             <div class=" grid grid-cols-2 hover:text-black duration-100">
-                                                <div class="">
+                                                {/* <div class="">
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                                         <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                                     </svg>
 
 
-                                                </div>
+                                                </div> */}
                                                 <div class="self-center text-lg">
                                                     Delete
                                                 </div>
